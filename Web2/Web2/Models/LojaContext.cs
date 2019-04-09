6b6f9328d6c615f90c4410fd422c81f9dbab2bc5
Thiagosouzamarc <35346674+Thiagosouzamarc@users.noreply.github.com>
2019-04-08 23:17:05 -0300

    08-04

---
+++ Web2/Web2/Models/LojaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Web2.Models
+{
+    public class LojaContext : DbContext
+    {
+        public LojaContext() : base("Loja")
+        {
+            Database.Log = instrucao => System.Diagnostics.Debug.WriteLine(instrucao);
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Consultor> Consultores { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+    }
+}
